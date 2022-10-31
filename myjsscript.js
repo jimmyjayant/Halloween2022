e198520233bbb07cf3472d82459631ccac4b0ac3
JIMMY JAYANT <113120690+jimmyjayant@users.noreply.github.com>
2022-11-01 01:23:08 +0530

    Add files via upload

---
+++ myjsscript.js
@@ -0,0 +1,29 @@
+function win2018() {
+    document.getElementById('win1').src = "Images/winner-2018-1.webp";
+    document.getElementById('win2').src = "Images/winner-2018-2.webp";
+    document.getElementById('win3').src = "Images/winner-2018-3.jpg";
+}
+
+function win2019() {
+    document.getElementById('win1').src = "Images/winner-2019-1.png";
+    document.getElementById('win2').src = "Images/winner-2019-2.webp";
+    document.getElementById('win3').src = "Images/winner-2019-3.jpg";
+}
+
+function win2020() {
+    document.getElementById('win1').src = "Images/winner-2020-1.webp";
+    document.getElementById('win2').src = "Images/winner-2020-2.webp";
+    document.getElementById('win3').src = "Images/winner-2020-3.webp";
+}
+
+function win2021() {
+    document.getElementById('win1').src = "Images/winner-2021-1.jpg";
+    document.getElementById('win2').src = "Images/winner-2021-2.webp";
+    document.getElementById('win3').src = "Images/winner-2021-3.jpg";
+}
+
+function win2022() {
+    document.getElementById('win1').src = "Images/winner-2022.jpg";
+    document.getElementById('win2').src = "Images/winner-2022.jpg";
+    document.getElementById('win3').src = "Images/winner-2022.jpg";
+}
