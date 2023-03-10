5e1df354f3e0476371fc6e0bd0650d483df55fea
JIMMY JAYANT <113120690+jimmyjayant@users.noreply.github.com>
2023-03-10 21:20:26 +0530

    Update myjsscript.js
    Make the following changes:- 
    => Assign img elements to a const variable x,y,z. After that, change their src attribute. This helps in speeding up the script runtime.

--- myjsscript.js
+++ myjsscript.js
@@ -1,29 +1,33 @@
+const x = document.getElementById("win1");
+const y = document.getElementById("win2");
+const z = document.getElementById("win3");
+
 function win2018() {
-    document.getElementById('win1').src = "Images/winner-2018-1.webp";
-    document.getElementById('win2').src = "Images/winner-2018-2.webp";
-    document.getElementById('win3').src = "Images/winner-2018-3.jpg";
+    x.src = "Images/winner-2018-1.webp";
+    y.src = "Images/winner-2018-2.webp";
+    z.src = "Images/winner-2018-3.jpg";
 }
 
 function win2019() {
-    document.getElementById('win1').src = "Images/winner-2019-1.png";
-    document.getElementById('win2').src = "Images/winner-2019-2.webp";
-    document.getElementById('win3').src = "Images/winner-2019-3.jpg";
+    x.src = "Images/winner-2019-1.png";
+    y.src = "Images/winner-2019-2.webp";
+    z.src = "Images/winner-2019-3.jpg";
 }
 
 function win2020() {
-    document.getElementById('win1').src = "Images/winner-2020-1.webp";
-    document.getElementById('win2').src = "Images/winner-2020-2.webp";
-    document.getElementById('win3').src = "Images/winner-2020-3.webp";
+    x.src = "Images/winner-2020-1.webp";
+    y.src = "Images/winner-2020-2.webp";
+    z.src = "Images/winner-2020-3.webp";
 }
 
 function win2021() {
-    document.getElementById('win1').src = "Images/winner-2021-1.jpg";
-    document.getElementById('win2').src = "Images/winner-2021-2.webp";
-    document.getElementById('win3').src = "Images/winner-2021-3.jpg";
+    x.src = "Images/winner-2021-1.jpg";
+    y.src = "Images/winner-2021-2.webp";
+    z.src = "Images/winner-2021-3.jpg";
 }
 
 function win2022() {
-    document.getElementById('win1').src = "Images/winner-2022.jpg";
-    document.getElementById('win2').src = "Images/winner-2022.jpg";
-    document.getElementById('win3').src = "Images/winner-2022.jpg";
+    x.src = "Images/winner-2022.jpg";
+    y.src = "Images/winner-2022.jpg";
+    z.src = "Images/winner-2022.jpg";
 }
