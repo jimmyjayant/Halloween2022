5bb3b1afd01c373079e3121e11b91ce66f581d35
JIMMY JAYANT <113120690+jimmyjayant@users.noreply.github.com>
2025-01-23 16:23:35 +0530

    Move animated spider image gif to Images folder, modify the header and navbar of all pages, introduced the hamburger menu when the screen size is below 600px, delete upload.php and uploads folder, change css of footer to grid layout, and totally customize the css of index.html webpage.

--- myjsscript.js
+++ myjsscript.js
@@ -1,38 +1,63 @@
-window.onload = function() {
+/*window.onload = function() {
     const audio = new Audio("audio/scream.mp3");
     audio.play();
+}*/
+
+/* The function showmenu() is associated with hamburgermenu icon on top left corner of webpage. When you click on the icon, the navbar displays. */
+function showmenu() {
+    var navbar = document.getElementsByClassName("navbar")[0];
+    navbar.style.display = "block";
 }
 
-const x = document.getElementById("win1");
-const y = document.getElementById("win2");
-const z = document.getElementById("win3");
-
-function win2018() {
-    x.src = "Images/winner-2018-1.webp";
-    y.src = "Images/winner-2018-2.webp";
-    z.src = "Images/winner-2018-3.jpg";
+/* The function hidemenu() is associated with X sign in the navbar below 600px screen size. When you click on the X sign, the navbar becomes hidden that is display none. */
+function hidemenu() {
+    var navbar = document.getElementsByClassName("navbar")[0];
+    navbar.style.display = "none";
 }
 
-function win2019() {
-    x.src = "Images/winner-2019-1.png";
-    y.src = "Images/winner-2019-2.webp";
-    z.src = "Images/winner-2019-3.jpg";
-}
+window.addEventListener("resize", () => {
+    if(window.innerWidth > 600)
+    {
+        var navbar = document.getElementsByClassName("navbar")[0];
+        navbar.style.display = "";
+        navbar.style.width = "";
+    }
+});
 
-function win2020() {
-    x.src = "Images/winner-2020-1.webp";
-    y.src = "Images/winner-2020-2.webp";
-    z.src = "Images/winner-2020-3.webp";
-}
 
-function win2021() {
-    x.src = "Images/winner-2021-1.jpg";
-    y.src = "Images/winner-2021-2.webp";
-    z.src = "Images/winner-2021-3.jpg";
-}
-
-function win2022() {
-    x.src = "Images/winner-2022.jpg";
-    y.src = "Images/winner-2022.jpg";
-    z.src = "Images/winner-2022.jpg";
+if(window.location.pathname.includes("history.html"))
+{
+    const x = document.getElementById("win1");
+    const y = document.getElementById("win2");
+    const z = document.getElementById("win3");
+    
+    function win2018() {
+        x.src = "Images/winner-2018-1.webp";
+        y.src = "Images/winner-2018-2.webp";
+        z.src = "Images/winner-2018-3.jpg";
+    }
+    
+    function win2019() {
+        x.src = "Images/winner-2019-1.png";
+        y.src = "Images/winner-2019-2.webp";
+        z.src = "Images/winner-2019-3.jpg";
+    }
+    
+    function win2020() {
+        x.src = "Images/winner-2020-1.webp";
+        y.src = "Images/winner-2020-2.webp";
+        z.src = "Images/winner-2020-3.webp";
+    }
+    
+    function win2021() {
+        x.src = "Images/winner-2021-1.jpg";
+        y.src = "Images/winner-2021-2.webp";
+        z.src = "Images/winner-2021-3.jpg";
+    }
+    
+    function win2022() {
+        x.src = "Images/winner-2022.jpg";
+        y.src = "Images/winner-2022.jpg";
+        z.src = "Images/winner-2022.jpg";
+    }    
 }
